53c7db2f86b7f00615632897af692c3f8732333f
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-29 16:32:35 +0300

    /

--- src/test/java/by/itacademy/anastasiyashirochina/taf/sites/DominosTest.java
+++ src/test/java/by/itacademy/anastasiyashirochina/taf/sites/DominosTest.java
@@ -4,8 +4,6 @@ import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DominosTest {
